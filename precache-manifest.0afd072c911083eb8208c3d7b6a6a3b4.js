b2a0a255e636f7c3c2b8689a48593f07bd4612b5
aizatazhar <aizat.azharr@gmail.com>
2020-09-09 21:19:41 +0800

    Updates

---
+++ precache-manifest.0afd072c911083eb8208c3d7b6a6a3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af007e058adb4efcd283e76e09ba3eba",
+    "url": "/dna-converter/index.html"
+  },
+  {
+    "revision": "9fb95992ef2e8e68f3f5",
+    "url": "/dna-converter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "69f19452137bbec62617",
+    "url": "/dna-converter/static/js/2.b2499399.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dna-converter/static/js/2.b2499399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb95992ef2e8e68f3f5",
+    "url": "/dna-converter/static/js/main.027d8a00.chunk.js"
+  },
+  {
+    "revision": "bb0cd1d69fcf5aa96209",
+    "url": "/dna-converter/static/js/runtime-main.398d0067.js"
+  }
+]);
